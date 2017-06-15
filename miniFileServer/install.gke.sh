4be68189f2db622ba1d1e36976891353f906a704
David BENQUE <dbenque@amadeus.com>
2017-06-15 23:35:57 +0200

    add installation files for gke

---
+++ miniFileServer/install.gke.sh
@@ -0,0 +1,27 @@
+# FIRST you must update kubectl context with a command like:
+#
+#         gcloud container clusters get-credentials {clustername} --zone {zonename}
+# 
+# example: gcloud container clusters get-credentials cluster-1 --zone europe-west1-b
+#
+#
+
+GO_ENABLED=0 go build -a -installsuffix cgo -ldflags '-s' .
+if [ ! $? -eq 0 ]; then
+ echo -e "go build failed"
+ exit 1
+fi
+#docker build -t minifileserver:v0 .
+docker build -t gcr.io/$DEVSHELL_PROJECT_ID/minifileserver:v0 .
+if [ ! $? -eq 0 ]; then
+ echo -e "docker build failed"
+ exit 1
+fi
+echo "Pushing image to gcr.io/$DEVSHELL_PROJECT_ID"
+gcloud docker -- push gcr.io/$DEVSHELL_PROJECT_ID/minifileserver:v0
+if [ ! $? -eq 0 ]; then
+ echo -e "docker push failed"
+ exit 1
+fi
+echo "Deployement"
+kubectl apply -f <(eval "echo \"$(< mfs.gke.yaml)\"")
